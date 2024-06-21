e4adcf32e08b8bc2c2436d5e0c313132f6f623a9
Pasha Fistanto <pasha@fstn.top>
2024-06-21 15:30:08 +0200

    fix: socket path

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ fn init_layout(target_kb: &str) -> Result<()> {
         eprintln!("Env variable {} not found: {x}", XDG_RUNTIME_DIR);
         process::exit(1);
     });
-    let sock_path = Path::new(&xdg_dir).join("hypr").join(hypr_socket).join(SOCKET2_NAME);
+    let sock_path = Path::new(&xdg_dir).join("hypr").join(hypr_socket).join(SOCKET_NAME);
 
     let mut stream = UnixStream::connect(sock_path)?;
     stream.write_all(b"devices")?;
